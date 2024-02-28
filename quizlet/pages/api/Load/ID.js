f93e483d2a06ba5f0a03b989027cea080388547c
Zonde246 <72788587+Zonde246@users.noreply.github.com>
2024-02-28 22:53:34 +0530

    ef

--- quizlet/pages/api/Load/ID.js
+++ quizlet/pages/api/Load/ID.js
@@ -2,10 +2,15 @@ import { Quizes } from "@/mongod/mongod";
 import mongoose from "mongoose";
 export default async function handler(req, res) {
   // console.log(req.body.ID);
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
 
-  await Quizes.findOne({ _id: req.body.ID }).then((data) => {
-    res.status(200).json(data);
-  });
+  const id = mongoose.Types.ObjectId(req.body.ID);
 
+  const data = await Quizes.findById(id);
+  // console.log(data);
+  res.status(200).json(data);
+  // res.status(200).json({ ok: true });
   res.status(500).json({ ok: false });
 }
