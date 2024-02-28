5cca8ab8cb9109073f443832d016fe6d0a51449a
Zonde246 <72788587+Zonde246@users.noreply.github.com>
2024-02-28 22:48:15 +0530

    Update ID.js

--- quizlet/pages/api/Load/ID.js
+++ quizlet/pages/api/Load/ID.js
@@ -1,15 +1,11 @@
 import { Quizes } from "@/mongod/mongod";
 import mongoose from "mongoose";
 export default async function handler(req, res) {
-  console.log(req.body.ID);
+  // console.log(req.body.ID);
 
-  await Quizes.findOne({ _id: req.body.ID })
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .then((data) => {
-      res.status(500).json({ ok: false });
-    });
+  await Quizes.findOne({ _id: req.body.ID }).then((data) => {
+    res.status(200).json(data);
+  });
 
   res.status(500).json({ ok: false });
 }
